64b520f8c6b222c334b4acbfd18ec5418406076f
João <joaopaulokimi@gmail.com>
2024-08-26 14:46:12 -0300

    add: inicinado projeto spring boot

---
+++ src/main/java/me/joao/dev_joao_digital_bank_2024/DevJoaoDigitalBank2024Application.java
@@ -0,0 +1,13 @@
+package me.joao.dev_joao_digital_bank_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJoaoDigitalBank2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJoaoDigitalBank2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/joao/dev_joao_digital_bank_2024/DevJoaoDigitalBank2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joao.dev_joao_digital_bank_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJoaoDigitalBank2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
